2ab51c01b555321ee60447ac22b0306df83b016d
bofei <bofei222@163.com>
2019-01-10 22:55:32 +0800

    7

--- src/main/java/com/bf/io/Main2.java
+++ src/main/java/com/bf/io/Main2.java
@@ -11,7 +11,7 @@ public class Main2 {
     public static void main(String[] args) {
         int p1 = 5 * 1024 * 1024; //分段大小在 5MB - 5GB 之间，只有最后一个分段才允许小于 5MB，不可避免的
         int p2 = 3 * 1024 * 1024; //分段大小在 5MB - 5GB 之间，只有最后一个分段才允许小于 5MB，不可避免的
-        int p3 = 9 * 1024 * 1024; //分段大小在 5MB - 5GB 之间，只有最后一个分段才允许小于 5MB，不可避免的
+        int p3 = 3 * 1024 * 1024; //分段大小在 5MB - 5GB 之间，只有最后一个分段才允许小于 5MB，不可避免的
         byte[] b1 = RandomStringUtils.randomAlphabetic(p1).getBytes(); //填充一个 5MB 的字符串
         byte[] b2 = RandomStringUtils.randomAlphabetic(p2).getBytes(); //填充一个 5MB 的字符串
         byte[] b3 = RandomStringUtils.randomAlphabetic(p3).getBytes(); //填充一个 5MB 的字符串
@@ -19,11 +19,11 @@ public class Main2 {
         StorageConfig sc = new StorageConfig("s3");
         sc.init();
         StorageFile toS3 = new ToS3(sc);
-        boolean open = toS3.open("bofei的一个3M文件", "w");
+        boolean open = toS3.open("bofei的一个3M+3M文件", "w");
         IntHolder holder = new IntHolder();
 //        toS3.write(b1, 0, b1.length, holder);
         toS3.write(b2, 0, b2.length, holder);
-//        toS3.write(b3, 0, b3.length, holder);
+        toS3.write(b3, 0, b3.length, holder);
         toS3.close();
 
     }
